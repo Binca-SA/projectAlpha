e98bd5b00bf530a5f97c0a1ccbfc61db51b69e01
Binca-SA <binnas.shinaasi.asanova@gmail.com>
2022-01-07 18:24:55 +0100

    realización apartado FORTALEZAS

--- js/background.js
+++ js/background.js
@@ -8,13 +8,6 @@ change = () => {
                         rgba(220, 235, 206, 0.88) -12.82deg, #002F66 32.42deg, #D9D4EB 313.99deg, 
                         rgba(220, 235, 206, 0.88) 347.18deg, #002F66 392.42deg)`
 
-    // document.querySelector('#home').checked
-    // ? 
-    // () => {
-    //     document.body.style.backgroundColor = primary
-    //     document.body.style.backgroundImage = ''
-    // }
-    // :document.body.style.backgroundImage = secundary
 
     
     if( document.querySelector('#home').checked ){
